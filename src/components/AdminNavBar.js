6295ee7198fdd4fe39d5a0450aee774368984de2
AnitaNamjoon <135642350+AnitaNamjoon@users.noreply.github.com>
2023-08-01 18:40:56 +0300

    Update AdminNavBar.js

--- src/components/AdminNavBar.js
+++ src/components/AdminNavBar.js
@@ -1,6 +1,12 @@
 import React from "react";
 
 function AdminNavBar({ onChangePage }) {
+  const handleNewQuestionClick = () => {
+    onChangePage('Form'); 
+  };
+
+  const handleViewQuestionsClick = () => {// Call the onChangePage function with 'List' as an argument
+  };
   return (
     <nav>
       <button onClick={() => onChangePage("Form")}>New Question</button>
